63538ae46106b001719bf0572330f651d17f211f
asunm1483212204805 <144371930+asunm1483212204805@users.noreply.github.com>
2023-10-16 19:25:16 +0530

    Delete main (1).py

--- main (1).py
+++
@@ -1,9 +0,0 @@
-# 1.1 IMPLEMENT A RECURSIVE FUNCTION TO CALCULATE THE FACTORIAL OF A GIVEN NUMBER.
-def factorial(n):
-  if n==0:
-    return 1
-  else:
-    return n*factorial(n-1)
-
-for i in range(10):
-   print(factorial(i))  
